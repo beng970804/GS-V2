b566284817679e45e80f98086733e57e85edaf0c
beng970804 <beng970804@gmail.com>
2024-08-12 22:22:57 +0800

    chore: update font

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import {Text} from '@/components/ui/text';
 export const AppScreen = () => {
   return (
     <View>
-      <Text className="text-dxl text-error-700">text 1</Text>
-      <Text className="text-error-700">text 2</Text>
-      <Text className="text-dxl">text 3</Text>
+      <Text className="text-sm text-error-700">text 1</Text>
+      <Text className="text-md text-error-700">text 2</Text>
     </View>
   );
 };
