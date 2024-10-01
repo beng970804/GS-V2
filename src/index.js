d8dfffc6f21177942c8e5eebde91e234975fe6b9
beng970804 <beng970804@gmail.com>
2024-10-01 20:34:52 +0800

    chore: revert nativewind version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import {ChevronDownIcon, Icon} from '@/components/ui/icon';
+import {Menu, MenuItem, MenuItemLabel} from '@/components/ui/menu';
 import {Text} from '@/components/ui/text';
 import {View} from '@/components/ui/view';
 
@@ -7,6 +9,20 @@ export const AppScreen = () => {
       <Text className="font-heading text-sm text-error-700">text 1</Text>
       <Text className="font-body text-md text-error-700">text 2</Text>
       <Text className="font-mono text-md text-error-700">text 2</Text>
+      <Menu
+        offset={5}
+        trigger={({...triggerProps}) => {
+          return (
+            <Icon as={ChevronDownIcon} className="m-2 h-4 w-4 text-error-500" />
+          );
+        }}>
+        <MenuItem
+          key="Membership"
+          textValue="Membership"
+          className="justify-between p-2">
+          <MenuItemLabel size="sm">Membership</MenuItemLabel>
+        </MenuItem>
+      </Menu>
     </View>
   );
 };
